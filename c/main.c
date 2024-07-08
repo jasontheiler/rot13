9ec6dbdef84147017398c50c94fe61f1b799e087
Jason Theiler <turret.jason@outlook.com>
2024-07-08 14:41:59 +0200

    refactor(c): use different pointer declaration syntax

--- c/main.c
+++ c/main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void rot13(char *buf, int n) {
+void rot13(char* buf, int n) {
 	char a;
 	for (size_t i = 0; i < n; i++) {
 		if (buf[i] >= 'a' && buf[i] <= 'z') {
@@ -17,13 +17,13 @@ void rot13(char *buf, int n) {
 	}
 }
 
-int from_args(int argc, char **argv) {
+int from_args(int argc, char** argv) {
 	for (size_t i = 1; i < argc; i++) {
 		rot13(argv[i], strlen(argv[i]));
 		if (fputs(argv[i], stdout) == EOF) {
 			return EXIT_FAILURE;
 		}
-		char *c;
+		char* c;
 		if (i < argc - 1) {
 			c = " ";
 		} else {
@@ -53,7 +53,7 @@ int from_stdin() {
 	return EXIT_SUCCESS;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
 	if (argc > 1) {
 		return from_args(argc, argv);
 	}
